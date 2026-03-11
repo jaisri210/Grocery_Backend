1b5da89b4cb8d63c6984bc2323086e4dfa0df2f8
jaisri <jaisriarangasamy@gmail.com>
2026-03-11 18:23:43 +0530

    add toggle user status

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,4 +50,21 @@ const getAllUsers = async (req, res) => {
     });
   }
 };
-module.exports = { loginUser, getUserById, getAllUsers };
+const toggleUserStatus = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.isActive = !user.isActive;
+
+    await user.save();
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { loginUser, getUserById, getAllUsers, toggleUserStatus };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,8 @@ const {
 const { protect, admin } = require("../middleware/authMiddleware");
 
 router.post("/login", loginUser);
-router.get("/", protect, admin, getAllUsers); // 👈 THIS FIXES YOUR 404
+router.get("/", protect, admin, getAllUsers);
 router.get("/:id", protect, admin, getUserById);
+router.put("/:id/toggle", protect, admin, toggleUserStatus);
 
 module.exports = router;
